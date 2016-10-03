4bf2525ab680ebf4a8889cec80819652c64f87ec
jacob4684 <jaco4684@CV-PC-S-02>
2016-10-03 14:20:51 +0200

    static Logger

---
+++ Networking/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    public static class Logger
+    {
+        readonly static string path;
+        
+
+
+        static Logger()
+        {
+            path = "vejen til";
+        }
+        public static void Log(string message, bool OmitDate)
+        {
+            if (!OmitDate)
+                message = DateTime.Now + " " + message;
+            Trace.WriteLine(message);
+        }
+        static void Log(string message) { Log(message, false); }
+    }
+}
--- Services/Program.cs
+++ Services/Program.cs
@@ -10,6 +10,7 @@ namespace Services
     {
         static void Main(string[] args)
         {
+            Networking.Logger();
         }
     }
 }
