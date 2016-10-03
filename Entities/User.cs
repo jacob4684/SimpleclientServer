70e132dc55d735204ffb35634ca5e417396335f4
jacob4684 <jaco4684@CV-PC-S-02>
2016-10-03 14:50:07 +0200

    public class User

---
+++ Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class User
+    {
+        #region Fields
+        private string userName;
+        private string password;
+        #endregion
+        public User(string userName,string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+        #region Properties
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        #endregion
+
+    }
+}
