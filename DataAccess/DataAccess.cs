c0ff227466a833047e4d880c742761401d60f06c
jacob4684 <jaco4684@edu.campusvejle.dk>
2016-10-05 13:45:08 +0200

    fix commit problem

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataAccess
+    {
+        private string connectionString;
+        private SqlConnection con;
+        private SqlCommand command;
+        public DataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public DataAccess()
+        {
+            connectionString = @"Server=CV-PC-S-02\SQLEXPRESS;Database=FlydeKasser;Trusted_Connection=True;";
+        }
+        protected void SetCon(string strSqlCon)
+        {
+            connectionString = strSqlCon;
+            con = new SqlConnection(this.connectionString);
+        }
+        private void CloseDB()
+        {
+            try
+            {
+                if (this.con != null)
+                {
+                    this.con.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                this.con = null;
+            }
+        }
+        private void OpenDB()
+        {
+            try
+            {
+                if (con != null && con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                else
+                {
+                    CloseDB();
+                    OpenDB();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+
+            }
+        }
+        protected int ExecuteNonQuery(string sql)
+        {
+            int result = 0;
+            this.con = new SqlConnection(this.connectionString);
+            this.command = new SqlCommand(sql, this.con);
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                this.CloseDB();
+            }
+            return result;
+        }
+        protected SqlDataReader ExecuteReader(string sql)
+        {
+            SqlDataReader reader = null;
+            this.con = new SqlConnection(this.connectionString);
+            this.command = new SqlCommand(sql, this.con);
+            try
+            {
+                OpenDB();
+                reader = this.command.ExecuteReader();
+            }
+            catch (Exception)
+            {
+
+            }
+            return reader;
+        }
+        protected DataTable DbReturnDataTable(string strSql)
+        {
+            DataTable dtRes = new DataTable();
+            try
+            {
+                OpenDB();
+                using (var command = new SqlCommand(strSql, this.con))
+                using (var adapter = new SqlDataAdapter(command))
+                {
+                    adapter.Fill(dtRes);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                CloseDB();
+            }
+            return dtRes;
+        }
+        protected List<string> DbReturnListString(string strSql)
+        {
+            List<string> strList = new List<string>();
+            try
+            {
+                using (var cmd = con.CreateCommand())
+                {
+                    OpenDB();
+                    cmd.CommandText = strSql;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                strList.Add(reader.GetValue(i).ToString());
+                            }
+                        }
+                    }
+
+                    CloseDB();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            return strList;
+        }
+    }
+}
---
+++ DataAccess/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class Logger
+    {
+        /// <summary>
+        /// the place where the log is save on the pc
+        /// </summary>
+        private readonly static string Path;
+
+        static Logger()
+        {
+            try
+            {
+                string codebase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codebase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                Path = System.IO.Path.GetDirectoryName(path);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        public static void Log(string message)
+        {
+            if (message != null)
+            {
+                DateTime dateTimeNow = new DateTime();
+                dateTimeNow = DateTime.Now;
+                File.WriteAllText(Path, dateTimeNow.ToString("f", CultureInfo.CreateSpecificCulture("da-Dk")) + message);
+            }
+
+        }
+
+    }
+}
