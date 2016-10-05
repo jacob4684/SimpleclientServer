5a340731bb7f9835928b2d408905146d71c5788f
jacob4684 <jaco4684@edu.campusvejle.dk>
2016-10-05 11:54:59 +0200

    insert code in Log metode

--- Controller/Class1.cs
+++ Controller/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,7 @@ namespace Controller
 {
     public class Class1
     {
+        
+        
     }//
 }
